7049cbc652d6180e6188d00ec0268af80f4a26e3
Koji Komiya <koukomdev@gmail.com>
2015-06-16 14:58:54 +0900

    modify mysql syori

--- recipes/mysql-server.rb
+++ recipes/mysql-server.rb
@@ -7,27 +7,16 @@ end
 
 # mysql-devel is necessary for "rails -d mysql"
 %w{mysql-community-server mysql-community-devel}.each do |pkg|
-  template "/etc/my.cnf" do
-    source "mysql-server/my.cnf.erb"
-    owner "root"
-    group "root"
-    mode 0644
-    action :nothing
-  end
-
-  template "/usr/share/mysql/charsets/Index.xml" do
-    source "mysql-server/charsets.xml.erb"
-    owner "root"
-    group "root"
-    mode 0644
-    action :nothing
-  end
-
   package pkg do
     action :install
-    notifies :create, resources( :template => "/etc/my.cnf" )
-    notifies :create, resources( :template => "/usr/share/mysql/charsets/Index.xml" )
   end
+end
+
+template "/etc/my.cnf" do
+  source "mysql-server/my.cnf.erb"
+  owner "root"
+  group "root"
+  mode 0644
 end
 
 service "mysqld" do

1b72951bb097f35927d4a82c3c88caee45861c58
Koji Komiya <koukomdev@gmail.com>
2016-04-14 19:53:53 +0900

    neobundle pathの修正

--- recipes/vim-neobundle.rb
+++ recipes/vim-neobundle.rb
@@ -2,10 +2,15 @@ include_recipe 'vagrant-env::vim'
 
 default_user = data_bag_item("users", "default")
 
-directory "#{default_user['home']}/.vim" do
-  user default_user["name"]
-  group default_user["name"]
-  action :create
+%W[
+  #{default_user['home']}/.vim
+  #{default_user['home']}/.vim/bundle
+].each do |path|
+  directory path do
+    user default_user["name"]
+    group default_user["name"]
+    action :create
+  end
 end
 
 git "#{default_user['home']}/.vim/bundle/neobundle.vim" do
@@ -14,7 +19,7 @@ git "#{default_user['home']}/.vim/bundle/neobundle.vim" do
 end
 
 execute "neobundle install" do
-  command "su #{default_user["name"]} -l -c \"#{default_user['home']}/.vim/neobundle.vim.git/bin/neoinstall 1>/dev/null 2>&1\""
+  command "su #{default_user["name"]} -l -c \"#{default_user['home']}/.vim/bundle/neobundle.vim/bin/neoinstall 1>/dev/null 2>&1\""
   only_if do
     /(yes|^y)/i =~ node[:vim][:neobundle][:exec]
   end
